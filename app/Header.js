6268ea78e27bdca8ab6750014911037b41b8667d
swari <swari21@navgurukul.org>
2023-11-03 13:36:14 +0530

    create header

---
+++ app/Header.js
@@ -0,0 +1,23 @@
+const Header=()=>{
+    return(
+        <>
+            <div class="conten">
+                <div class="row1"></div>
+                <div class="row2">
+                   <img class="logo"src="icon - Logo.svg"></img> 
+                   <img class="a"src="Movie Hub.svg"/>
+                   <img class="b"src="Watch.svg"/>
+                   <img class="c"src="Add Movie.svg"/>
+                </div>
+                <div class="row3"></div>
+                <div class="row4"></div>
+                <div class="row5">
+                    <p>Watch</p>
+                </div>
+           </div>
+
+       </>
+    )
+}
+
+export default Header;
